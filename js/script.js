8cdbb44a1dc2512dd17a7462f08e932ea26be624
Hubert Kozłowski <hubert890@poczta.onet.pl>
2018-09-14 15:38:43 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
 
 
 function initLocalClocks() {
@@ -37,110 +37,55 @@ function initLocalClocks() {
 	}
 }
 
-// function setUpMinuteHands() {
-//
-// 	  var container = document.querySelector('.minutes-container');
-// 	  var secondAngle = container.getAttribute("data-second-angle");
-// 	  if (secondAngle > 0) {
-//
-// 		    var delay = (((360 - secondAngle) / 6) + 0.1) * 1000;
-// 		    setTimeout(function() {
-// 		      	moveMinuteHands(container);
-// 		    }, delay);
-// 	  }
-// }
-
-// function moveMinuteHands(containers) {
-//
-// 		    containers.style.webkitTransform = 'rotateZ(6deg)';
-// 		    containers.style.transform = 'rotateZ(6deg)';
-//
-// 	  setInterval(function() {
-//
-// 	      if (containers.angle === undefined) {
-// 	        	containers.angle = 12;
-// 	      } else {
-// 	        	containers.angle += 6;
-// 	      }
-// 	      containers.style.webkitTransform = 'rotateZ('+ containers.angle +'deg)';
-// 	      containers.style.transform = 'rotateZ('+ containers.angle +'deg)';
-//
-// 	  }, 60000);
-// }
-//
-// function moveSecondHands() {
-// 	  var container = document.querySelector('.seconds-container');
-// 	  setInterval(function() {
-//
-// 	      if (container.angle === undefined) {
-// 	        	container.angle = 6;
-// 	      } else {
-// 	        	container.angle += 6;
-// 	      }
-//
-// 	      container.style.webkitTransform = 'rotateZ('+ container.angle +'deg)';
-// 	      container.style.transform = 'rotateZ('+ container.angle +'deg)';
-//
-// 	  }, 1000);
-// }
-
-/*
- * Set a timeout for the first minute hand movement (less than 1 minute), then rotate it every minute after that
- */
 function setUpMinuteHands() {
-  // Find out how far into the minute we are
-  var containers = document.querySelectorAll('.minutes-container');
-  var secondAngle = containers[0].getAttribute("data-second-angle");
-  if (secondAngle > 0) {
-    // Set a timeout until the end of the current minute, to move the hand
-    var delay = (((360 - secondAngle) / 6) + 0.1) * 1000;
-    setTimeout(function() {
-      moveMinuteHands(containers);
-    }, delay);
-  }
+
+	  var container = document.querySelector('.minutes-container');
+	  var secondAngle = container.getAttribute("data-second-angle");
+	  if (secondAngle > 0) {
+
+		    var delay = (((360 - secondAngle) / 6) + 0.1) * 1000;
+		    setTimeout(function() {
+		      	moveMinuteHands(container);
+		    }, delay);
+	  }
 }
 
-/*
- * Do the first minute's rotation
- */
 function moveMinuteHands(containers) {
-  for (var i = 0; i < containers.length; i++) {
-    containers[i].style.webkitTransform = 'rotateZ(6deg)';
-    containers[i].style.transform = 'rotateZ(6deg)';
-  }
-  // Then continue with a 60 second interval
-  setInterval(function() {
-    for (var i = 0; i < containers.length; i++) {
-      if (containers[i].angle === undefined) {
-        containers[i].angle = 12;
-      } else {
-        containers[i].angle += 6;
-      }
-      containers[i].style.webkitTransform = 'rotateZ('+ containers[i].angle +'deg)';
-      containers[i].style.transform = 'rotateZ('+ containers[i].angle +'deg)';
-    }
-  }, 60000);
+
+		    containers.style.webkitTransform = 'rotateZ(6deg)';
+		    containers.style.transform = 'rotateZ(6deg)';
+
+	  setInterval(function() {
+
+	      if (containers.angle === undefined) {
+	        	containers.angle = 12;
+	      } else {
+	        	containers.angle += 6;
+	      }
+	      containers.style.webkitTransform = 'rotateZ('+ containers.angle +'deg)';
+	      containers.style.transform = 'rotateZ('+ containers.angle +'deg)';
+
+	  }, 60000);
 }
 
-/*
- * Move the second containers
- */
 function moveSecondHands() {
-  var containers = document.querySelectorAll('.seconds-container');
-  setInterval(function() {
-    for (var i = 0; i < containers.length; i++) {
-      if (containers[i].angle === undefined) {
-        containers[i].angle = 6;
-      } else {
-        containers[i].angle += 6;
-      }
-      containers[i].style.webkitTransform = 'rotateZ('+ containers[i].angle +'deg)';
-      containers[i].style.transform = 'rotateZ('+ containers[i].angle +'deg)';
-    }
-  }, 1000);
+	  var container = document.querySelector('.seconds-container');
+	  setInterval(function() {
+
+	      if (container.angle === undefined) {
+	        	container.angle = 6;
+	      } else {
+	        	container.angle += 6;
+	      }
+
+	      container.style.webkitTransform = 'rotateZ('+ container.angle +'deg)';
+	      container.style.transform = 'rotateZ('+ container.angle +'deg)';
+
+	  }, 1000);
 }
 
 initLocalClocks();
 moveSecondHands();
 setUpMinuteHands();
-// })
+
+})
